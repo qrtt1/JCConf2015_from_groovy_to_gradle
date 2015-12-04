18b47ab40320d17323e1778a2df91859fbeb4433
Ching Yi, Chan <chingyichan.tw@gmail.com>
2015-12-04 11:04:06 +0800

    update access by properties

--- examples/001_bean.groovy
+++ examples/001_bean.groovy
@@ -1,15 +1,18 @@
 class Bean {
     String name;
+    String someVar;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public void setName(String name) { this.name = name; }
+    public String getName() { return this.name; }
 
-    public String getName() {
-        return this.name;
-    }
+    public String getValue() { return someVar; }
+    public void setValue(String value) { this.someVar = value; }
+
 }
 
 def bean = new Bean();
 bean.setName("hello gradle");
 println bean.getName();
+
+bean.value = "123";
+println bean.value
